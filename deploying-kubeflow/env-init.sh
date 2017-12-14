99c1271173f83cf298afcdacf3a981ea9daeafa2
Ben Hall <ben@benhall.me.uk>
2017-12-14 11:58:22 +0000

    Run on node

--- deploying-kubeflow/env-init.sh
+++ deploying-kubeflow/env-init.sh
@@ -1,4 +1,1 @@
-mkdir kubeflow
-git clone https://github.com/katacoda/kubeflow kubeflow
-echo "  externalIPs:" >> kubeflow/components/jupyterhub/manifests/service.yaml
-echo "  - 172.17.0.12" >> kubeflow/components/jupyterhub/manifests/service.yaml
+ssh root@host01 "mkdir kubeflow && git clone https://github.com/katacoda/kubeflow kubeflow && echo '  externalIPs:' >> kubeflow/components/jupyterhub/manifests/service.yaml && echo '  - 172.17.0.12' >> kubeflow/components/jupyterhub/manifests/service.yaml"
